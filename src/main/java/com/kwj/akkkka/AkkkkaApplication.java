56ad1234d84c903d45f97d43d11e69f23057cb69
woojae9488 <woojae9488@naver.com>
2022-01-13 22:47:24 +0900

    init project

---
+++ src/main/java/com/kwj/akkkka/AkkkkaApplication.java
@@ -0,0 +1,13 @@
+package com.kwj.akkkka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AkkkkaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AkkkkaApplication.class, args);
+    }
+
+}
